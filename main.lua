bed11ff306f7f52b3db326fe547afd12eb53e388
Dominik Scherer <dominik@scherer.de>
2014-11-14 23:38:51 +0100

    renamed HymnOfSnowAndMagma to hymn

--- main.lua
+++ main.lua
@@ -3,11 +3,9 @@ local baseWidth, baseHeight = 1920, 1080
 local mouseCursor
 local fullscreen = false
 
-
-
 function love.load(arg)
     if arg[3] == "hymn" then
-        require "HymnOfSnowAndMagma.main"()
+        require "hymn.main"()
     else
         print "get a main, biatches!"
     end
