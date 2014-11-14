3c9cce10eb191cb1237e99d78af56536b7331a55
Dominik Scherer <dominik@scherer.de>
2014-11-14 23:36:17 +0100

    ..

--- HymnOfSnowAndMagma/main.lua
+++ HymnOfSnowAndMagma/main.lua
@@ -1,7 +1,4 @@
-local screen = SplashScreen
 local baseWidth, baseHeight = 1920, 1080
-local mouseCursor
-local fullscreen = false
 
 local function load()
     mouseCursor = love.graphics.newImage("images/ui/mouseCursor.png")
@@ -22,22 +19,18 @@ function love.keypressed(key, unicode)
 end
 
 function love.mousepressed(x, y, button)
-    screen.mousepressed(x,y, button)
 end
 
 function love.mousereleased(x, y, button)
-    screen.mousereleased(x,y, button)
 end
 
 function love.focus(focussed)
 end
 
 function love.textinput(text)
-    GUI.textinput(text)
 end
 
 function love.resize(w, h)
-    GUI.resize(w, h)
 end
 
 return load
--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ local baseWidth, baseHeight = 1920, 1080
 local mouseCursor
 local fullscreen = false
 
+
+
 function love.load(arg)
     if arg[3] == "hymn" then
         require "HymnOfSnowAndMagma.main"()
