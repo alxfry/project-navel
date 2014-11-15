059696b3a5b2c7dbe11e3605512284e9b33fb3e0
Dominik Scherer <dominik@scherer.de>
2014-11-15 02:33:58 +0100

    Renamed project

--- conf.lua
+++ conf.lua
@@ -4,7 +4,7 @@
 -- https://love2d.org/wiki/Config_Files
 
 function love.conf(t)
-    t.title = "Switcheroo"
+    t.title = "SpiceMinons Engine"
     t.version = "0.9.0" -- results in a warning if the user has a different version of Love2D installed
     t.console = false -- Attach a console (boolean, Windows only)
     t.window.fullscreen = false
