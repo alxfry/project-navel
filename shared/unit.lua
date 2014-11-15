52a76fb92603f63cd3fa97678512f86b637b8577
Dominik Scherer <dominik@scherer.de>
2014-11-15 01:11:49 +0100

    stable speed unit movement

--- shared/unit.lua
+++ shared/unit.lua
@@ -4,18 +4,22 @@ local Entity = require "shared.entity"
 local Unit = Entity:subclass("Unit")
 
 -- speed: pixels/second
-function Unit:initialize(speed)
+-- direction: radians
+function Unit:initialize(speed, direction)
     Entity.initialize(self)
 
     self.speed = speed
+    self.direction = direction
     self.targetPosition = GameMath.Vector2.new(self.position.x, self.position.y)
 end
 
 function Unit:update(dt)
-    print(self.position.x, self.position.y)
-    local direction = self.targetPosition - self.position
-    direction:normalize()
-    self.position = self.position + direction * dt * self.speed
+    local direction = (self.targetPosition - self.position)
+    local length = GameMath.Vector2.length(direction)
+    if length > 1 then
+        direction = direction/length
+        self.position = self.position + direction * dt * self.speed
+    end
 end
 
 function Unit:moveTo(x, y)
