f7ea8cd409afb194d322263eb208fa49282e103d
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2014-11-15 19:44:47 +0100

    Render minion master health

--- minionmaster/ui.lua
+++ minionmaster/ui.lua
@@ -12,12 +12,20 @@ function ui.update(dt)
 end
 
 function ui.draw()
+    local width, height = love.graphics.getDimensions()
+
     -- DNA
     love.graphics.setFont(mediumFont)
     love.graphics.setColor(0, 0, 0)
     love.graphics.print("DNA: " .. state.dna, 12, 12)
-    love.graphics.setColor(255, 255, 255)
+    love.graphics.setColor(100, 100, 255)
     love.graphics.print("DNA: " .. state.dna, 10, 10)
+
+    -- Master Health
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("Health: " .. state.master.health, 12, height - 55)
+    love.graphics.setColor(255, 100, 100)
+    love.graphics.print("Health: " .. state.master.health, 10, height - 57)
 end
 
 return ui
