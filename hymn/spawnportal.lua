691ea3f8ccf9a8eb927d0769ac79ab054a94cf4d
Dominik Scherer <dominik@scherer.de>
2014-11-15 15:55:03 +0100

    Path functionality for Spawn portal

--- hymn/spawnportal.lua
+++ hymn/spawnportal.lua
@@ -3,6 +3,7 @@ local SpawnPortal = Building:subclass("SpawnPortal")
 
 function SpawnPortal:initialize()
     Building.initialize(self)
+    self.path = {}
 end
 
 local themes = {
@@ -14,6 +15,14 @@ function SpawnPortal:setPlayer(player)
     Building:setPlayer(player)
     self.theme = themes[player.playerId] or themes[1]
     self:setAnimation("images/buildings/" .. self.theme .. "/portal.png", 0.1)
+end
+
+function SpawnPortal:addPathPoint(position)
+    self.path[#self.path + 1] = position
+end
+
+function SpawnPortal:clearPath()
+    self.path = {}
 end
 
 return SpawnPortal
