7fc9bd37cb43a0399c0fbf6dc02ba5f4f28dac91
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2014-11-16 13:46:36 +0100

    Shared health bars with Minion Master: Eat or be Eaten

---
+++ shared/ui.lua
@@ -0,0 +1,15 @@
+local ui = {}
+
+function ui.drawHealthBar(entity)
+    local width = 40
+    local height = 5
+    local x = entity.position.x - width/2
+    local y = entity.position.y - 30
+    love.graphics.setColor(0, 16, 0, 255)
+    love.graphics.rectangle("fill", x-1, y-1, width+2, height+2 )
+    love.graphics.setColor(0, 128, 0, 255)
+    love.graphics.rectangle("fill", x, y, width*entity.health/entity.maxHealth, height )
+    love.graphics.setColor(255, 255, 255, 255)
+end
+
+return ui
