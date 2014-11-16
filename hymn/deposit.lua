658e417c6d9cfe3e90d657767c24393d39541888
Dominik Scherer <dominik@scherer.de>
2014-11-16 03:10:37 +0100

    Claim ownership functionality for deposits

--- hymn/deposit.lua
+++ hymn/deposit.lua
@@ -1,15 +1,16 @@
 local Building = require "shared.building"
 local LogicCore = require "hymn.logiccore"
 local EntityStatics = require "hymn.staticdata.entitystatics"
+local GameMath = require "shared.gamemath"
 
 local Deposit = Building:subclass("Deposit")
 
 function Deposit:initialize(entityStatic)
     Building.initialize(self, entityStatic, player)
 
-    self.owners = {}
+    self.claims = {}
     for playerId, player in pairs(LogicCore.players) do
-        self.owners[playerId] = 0
+        self.claims[playerId] = 0
     end
 
     self:setAnimation("images/buildings/resourcesrock_neutral.png", 1)
@@ -26,6 +27,33 @@ function Deposit:update(dt)
     if somethingInterestingHappens then
         -- change ownership
         self:setAnimation("images/buildings/" .. self.theme .. "/resourcesrock.png", 0.1)
+    end
+end
+
+function Deposit:takeOwnership(player)
+    if self.owner then
+        self.owner = self.resource - 50
+    end
+
+    player.resource = player.resource + 50
+    self.owner = player
+end
+
+function Deposit:claim(entity, amount)
+    local playerId = entity.player.playerId
+    local maxClaim = 0
+    for id, amount in pairs(self.claims) do
+        local diff = (id == playerId) and amount or -amount
+        self.claims[id] = GameMath.clamp(self.claim[id] + diff, 0, 100)
+        if id ~= playerId then
+            maxClaim = math.max(maxClaim, self.claims[id])
+        end
+    end
+
+    local ownClaim = self.claims[playerId]
+
+    if maxClaim == 0 and ownClaim > 50 then
+        self:takeOwnership(entity.player)
     end
 end
 
