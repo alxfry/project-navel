fc15299059053c132c0286fe21eabbfb1d2d90df
Dominik Scherer <dominik@scherer.de>
2014-11-16 16:25:21 +0100

    Construction health bars

--- hymn/ui.lua
+++ hymn/ui.lua
@@ -49,7 +49,9 @@ function ui.draw()
     love.graphics.translate(inputHandler.translate.x, inputHandler.translate.y)
 
     for id, entity in pairs(entityManager.entities) do
-        if entity.health then
+        if entity.constructing then
+            sharedui.drawProgress(entity)
+        elseif entity.health then
             sharedui.drawHealthBar(entity)
         end
     end
--- shared/ui.lua
+++ shared/ui.lua
@@ -8,7 +8,19 @@ function ui.drawHealthBar(entity)
     love.graphics.setColor(0, 16, 0, 255)
     love.graphics.rectangle("fill", x-1, y-1, width+2, height+2 )
     love.graphics.setColor(0, 128, 0, 255)
-    love.graphics.rectangle("fill", x, y, width*entity.health/entity.maxHealth, height )
+    love.graphics.rectangle("fill", x, y, width*entity.health/entity.maxHealth, height)
+    love.graphics.setColor(255, 255, 255, 255)
+end
+
+function ui.drawProgress(entity)
+    local width = 40
+    local height = 5
+    local x = entity.position.x - width/2
+    local y = entity.position.y + 30
+    love.graphics.setColor(16, 16, 0, 255)
+    love.graphics.rectangle("fill", x-1, y-1, width+2, height+2 )
+    love.graphics.setColor(250, 233, 50, 255)
+    love.graphics.rectangle("fill", x, y, width*entity.health/entity.maxHealth, height)
     love.graphics.setColor(255, 255, 255, 255)
 end
 
